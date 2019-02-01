d8f37256f28679c9001c978a183d592af9aa0a16
simerplaha <simer.j@gmail.com>
2019-02-02 00:32:27 +1100

    replaced Try with IO

--- data/src/test/scala/swaydb/data/io/IOSpec.scala
+++ data/src/test/scala/swaydb/data/io/IOSpec.scala
@@ -22,8 +22,249 @@ package swaydb.data.io
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.exceptions.TestFailedException
+import scala.collection.mutable.ListBuffer
+import swaydb.data.io.IO.orNone
+import swaydb.data.slice.Slice
+import swaydb.data.util.ByteSizeOf
+import IO._
 
 class IOSpec extends WordSpec with Matchers with MockFactory {
+
+
+  "Catch" when {
+    "exception" in {
+      val exception = new Exception("Failed")
+      IO.Catch(throw exception).failed.get shouldBe exception
+    }
+
+    "no exception" in {
+      IO.Catch(IO.successNone).get shouldBe empty
+    }
+  }
+
+  "Slice.tryForeach" should {
+    "terminate in first failure returning the failure" in {
+      val slice = Slice(1, 2, 3, 4)
+      var iterations = 0
+
+      val result: Option[IO.Failure[Int]] =
+        slice.tryForeach {
+          item => {
+            iterations += 1
+            if (item == 3)
+              IO.Failure(new Exception(s"result at item $item"))
+            else
+              IO.Success(item)
+          }
+        }
+
+      result.isDefined shouldBe true
+      result.get.exception.getMessage shouldBe "result at item 3"
+      iterations shouldBe 3
+    }
+
+    "iterate all even on failure if failFast = false" in {
+      val slice = Slice(1, 2, 3, 4)
+      var iterations = 0
+
+      val result: Option[IO.Failure[Int]] =
+        slice.tryForeach(
+          item => {
+            iterations += 1
+            if (item == 3)
+              IO.Failure(new Exception(s"result at item $item"))
+            else {
+              IO.Success(item)
+            }
+          },
+          failFast = false
+        )
+
+      result.isDefined shouldBe true
+      result.get.exception.getMessage shouldBe "result at item 3"
+      iterations shouldBe 4
+    }
+
+    "finish iteration on IO.Success IO's" in {
+      val slice = Slice(1, 2, 3, 4)
+
+      val result: Option[IO.Failure[Int]] =
+        slice.tryForeach {
+          item =>
+            IO.Success(item)
+        }
+
+      result.isEmpty shouldBe true
+    }
+  }
+
+  "Slice.tryMap" should {
+
+    "allow map on Slice" in {
+      val slice = Slice(1, 2, 3, 4, 5)
+
+      val result: IO[Slice[Int]] =
+        slice.tryMap {
+          item =>
+            IO.Success(item + 1)
+        }
+
+      result.get.toArray shouldBe Array(2, 3, 4, 5, 6)
+    }
+
+    "allow map on Slice, terminating at first failure returning the failure and cleanUp should be invoked" in {
+      val slice = Slice(1, 2, 3, 4, 5)
+
+      val intsCleanedUp = ListBuffer.empty[Int]
+
+      val result: IO[Slice[Int]] =
+        slice.tryMap(
+          tryBlock = item => if (item == 3) IO.Failure(new Exception(s"Failed at $item")) else IO.Success(item),
+          recover = (ints: Slice[Int], _: IO.Failure[Slice[Int]]) => ints.foreach(intsCleanedUp += _)
+        )
+
+      result.isFailure shouldBe true
+      result.failed.get.getMessage shouldBe "Failed at 3"
+
+      intsCleanedUp should contain inOrderOnly(1, 2)
+    }
+  }
+
+  "Slice.tryFlattenIterable" should {
+
+    "return flattened list" in {
+      val slice = Slice(1, 2, 3, 4, 5)
+
+      val result: IO[Iterable[String]] =
+        slice.tryFlattenIterable {
+          item =>
+            IO(Seq(item.toString))
+        }
+
+      result.get.toArray shouldBe Array("1", "2", "3", "4", "5")
+    }
+
+    "return failure" in {
+      val slice = Slice(1, 2, 3, 4, 5)
+
+      val result: IO[Iterable[String]] =
+        slice.tryFlattenIterable {
+          item =>
+            if (item < 3) {
+              IO(Seq(item.toString))
+            }
+            else {
+              IO.Failure(new Exception("Kaboom!"))
+            }
+        }
+      result.failed.get.getMessage shouldBe "Kaboom!"
+    }
+  }
+
+  "Slice.tryFoldLeft" should {
+    "allow fold, terminating at first failure returning the failure" in {
+      val slice = Slice("one", "two", "three")
+
+      val result: IO[Int] =
+        slice.tryFoldLeft(0) {
+          case (count, item) =>
+            if (item == "two")
+              IO.Failure(new Exception(s"Failed at $item"))
+            else
+              IO.Success(count + 1)
+        }
+
+      result.isFailure shouldBe true
+      result.failed.get.getMessage shouldBe "Failed at two"
+    }
+
+    "allow fold on Slice" in {
+      val slice = Slice("one", "two", "three")
+
+      val result: IO[Int] =
+        slice.tryFoldLeft(0) {
+          case (count, _) =>
+            IO.Success(count + 1)
+        }
+
+      result.isSuccess shouldBe true
+      result.get shouldBe 3
+    }
+  }
+
+  "Slice.toArray of a sub slice" should {
+    "return the sub slice array only" in {
+      val slice = Slice.create[Byte](ByteSizeOf.int * 3).addInt(123).addInt(456).addInt(789)
+      slice.slice(0, 3).createReader().readInt() shouldBe 123
+      slice.slice(4, 7).createReader().readInt() shouldBe 456
+      slice.slice(8, 11).createReader().readInt() shouldBe 789
+    }
+  }
+
+  "Slice.tryUntilSome" should {
+    "return on first Successful result that resulted in Some value" in {
+      val slice = Slice(1, 2, 3, 4)
+      var iterations = 0
+
+      val result: IO[Option[(Int, Int)]] =
+        slice.tryUntilSome {
+          item => {
+            iterations += 1
+            if (item == 3)
+              IO.Success(Some(item))
+            else
+              IO.successNone
+          }
+        }
+
+      result.get shouldBe ((3, 3))
+      iterations shouldBe 3
+    }
+
+    "return None if all iterations return None" in {
+      val slice = Slice(1, 2, 3, 4)
+      var iterations = 0
+
+      val result: IO[Option[(Int, Int)]] =
+        slice.tryUntilSome {
+          _ => {
+            iterations += 1
+            IO.successNone
+          }
+        }
+
+      result.get shouldBe empty
+      iterations shouldBe 4
+    }
+
+    "terminate early on failure" in {
+      val slice = Slice(1, 2, 3, 4)
+      var iterations = 0
+
+      val result: IO[Option[(Int, Int)]] =
+        slice.tryUntilSome {
+          item => {
+            iterations += 1
+            IO.Failure(new Exception(s"Failed at $item"))
+          }
+        }
+
+      result.isFailure shouldBe true
+      result.failed.get.getMessage shouldBe "Failed at 1"
+      iterations shouldBe 1
+    }
+  }
+
+  "tryOrNone" when {
+    "exception" in {
+      orNone(throw new Exception("Failed")) shouldBe empty
+    }
+
+    "no exception" in {
+      orNone("success").get shouldBe "success"
+    }
+  }
+
 
   "Async" should {
     "flatMap on IO.Success" in {
