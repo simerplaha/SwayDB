d3ed7335710e5ee024aa3b477b884a471ffee47e
simerplaha <simer.j@gmail.com>
2019-08-03 13:17:12 +1000

    stress tests

--- core-stress/src/test/scala/swaydb/core/io/file/DBFileStressWriteSpec.scala
+++ core-stress/src/test/scala/swaydb/core/io/file/DBFileStressWriteSpec.scala
@@ -1,97 +1,97 @@
-///*
-// * Copyright (c) 2019 Simer Plaha (@simerplaha)
-// *
-// * This file is a part of SwayDB.
-// *
-// * SwayDB is free software: you can redistribute it and/or modify
-// * it under the terms of the GNU Affero General Public License as
-// * published by the Free Software Foundation, either version 3 of the
-// * License, or (at your option) any later version.
-// *
-// * SwayDB is distributed in the hope that it will be useful,
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// * GNU Affero General Public License for more details.
-// *
-// * You should have received a copy of the GNU Affero General Public License
-// * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
-// */
-//
-//package swaydb.core.io.file
-//
-//import swaydb.IOValues._
-//import swaydb.core.RunThis._
-//import swaydb.core.TestBase
-//import swaydb.core.TestData._
-//import swaydb.core.TestLimitQueues.fileOpenLimiter
-//import swaydb.core.util.Benchmark
-//import swaydb.data.util.StorageUnits._
-//
-//import scala.concurrent.Future
-//import scala.concurrent.duration._
-//
-//class DBFileStressWriteSpec extends TestBase with Benchmark {
-//
-//  implicit val limiter = fileOpenLimiter
-//
-//  "DBFile" should {
-//    //use a larger size (200000) to test on larger data-set.
-//    val bytes = randomByteChunks(size = 20000, sizePerChunk = 50.bytes)
-//
-//    "write key values to a ChannelFile" in {
-//      val path = randomFilePath
-//
-//      val file = DBFile.channelWrite(path, autoClose = false).runRandomIO.value
-//      benchmark("write 1 million key values to a ChannelFile") {
-//        bytes foreach {
-//          byteChunk =>
-//            file.append(byteChunk).runRandomIO.value
-//        }
-//      }
-//      file.close.runRandomIO.value
-//    }
-//
-//    "write key values to a ChannelFile concurrently" in {
-//      val path = randomFilePath
-//
-//      val file = DBFile.channelWrite(path, autoClose = false).runRandomIO.value
-//      benchmark("write 1 million key values to a ChannelFile concurrently") {
-//        Future.sequence {
-//          bytes map {
-//            chunk =>
-//              Future(file.append(chunk).runRandomIO.value)
-//          }
-//        } await 20.seconds
-//      }
-//      file.close.runRandomIO.value
-//    }
-//
-//    "write key values to a MMAPlFile" in {
-//      val path = randomFilePath
-//
-//      val file = DBFile.mmapInit(path, bytes.size * 50, autoClose = false).runRandomIO.value
-//      benchmark("write 1 million key values to a MMAPlFile") {
-//        bytes foreach {
-//          chunk =>
-//            file.append(chunk).runRandomIO.value
-//        }
-//      }
-//      file.close.runRandomIO.value
-//    }
-//
-//    "write key values to a MMAPlFile concurrently" in {
-//      val path = randomFilePath
-//
-//      val file = DBFile.mmapInit(path, bytes.size * 50, autoClose = false).runRandomIO.value
-//      benchmark("write 1 million key values to a MMAPlFile concurrently") {
-//        Future.sequence {
-//          bytes map {
-//            chunk =>
-//              Future(file.append(chunk).runRandomIO.value)
-//          }
-//        } await 20.seconds
-//      }
-//      file.close.runRandomIO.value
-//    }
-//  }
-//}
+/*
+ * Copyright (c) 2019 Simer Plaha (@simerplaha)
+ *
+ * This file is a part of SwayDB.
+ *
+ * SwayDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * SwayDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package swaydb.core.io.file
+
+import swaydb.IOValues._
+import swaydb.core.RunThis._
+import swaydb.core.TestBase
+import swaydb.core.TestData._
+import swaydb.core.TestLimitQueues.fileOpenLimiter
+import swaydb.core.util.Benchmark
+import swaydb.data.util.StorageUnits._
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+class DBFileStressWriteSpec extends TestBase with Benchmark {
+
+  implicit val limiter = fileOpenLimiter
+
+  "DBFile" should {
+    //use a larger size (200000) to test on larger data-set.
+    val bytes = randomByteChunks(size = 20000, sizePerChunk = 50.bytes)
+
+    "write key values to a ChannelFile" in {
+      val path = randomFilePath
+
+      val file = DBFile.channelWrite(path, randomIOAccess(true), autoClose = false).runRandomIO.value
+      benchmark("write 1 million key values to a ChannelFile") {
+        bytes foreach {
+          byteChunk =>
+            file.append(byteChunk).runRandomIO.value
+        }
+      }
+      file.close.runRandomIO.value
+    }
+
+    "write key values to a ChannelFile concurrently" in {
+      val path = randomFilePath
+
+      val file = DBFile.channelWrite(path, randomIOAccess(true), autoClose = false).runRandomIO.value
+      benchmark("write 1 million key values to a ChannelFile concurrently") {
+        Future.sequence {
+          bytes map {
+            chunk =>
+              Future(file.append(chunk).runRandomIO.value)
+          }
+        } await 20.seconds
+      }
+      file.close.runRandomIO.value
+    }
+
+    "write key values to a MMAPlFile" in {
+      val path = randomFilePath
+
+      val file = DBFile.mmapInit(path, randomIOAccess(true), bytes.size * 50, autoClose = false).runRandomIO.value
+      benchmark("write 1 million key values to a MMAPlFile") {
+        bytes foreach {
+          chunk =>
+            file.append(chunk).runRandomIO.value
+        }
+      }
+      file.close.runRandomIO.value
+    }
+
+    "write key values to a MMAPlFile concurrently" in {
+      val path = randomFilePath
+
+      val file = DBFile.mmapInit(path, randomIOAccess(true), bytes.size * 50, autoClose = false).runRandomIO.value
+      benchmark("write 1 million key values to a MMAPlFile concurrently") {
+        Future.sequence {
+          bytes map {
+            chunk =>
+              Future(file.append(chunk).runRandomIO.value)
+          }
+        } await 20.seconds
+      }
+      file.close.runRandomIO.value
+    }
+  }
+}
--- core-stress/src/test/scala/swaydb/core/segment/merge/SegmentMergeStressSpec.scala
+++ core-stress/src/test/scala/swaydb/core/segment/merge/SegmentMergeStressSpec.scala
@@ -1,123 +1,81 @@
-///*
-// * Copyright (c) 2019 Simer Plaha (@simerplaha)
-// *
-// * This file is a part of SwayDB.
-// *
-// * SwayDB is free software: you can redistribute it and/or modify
-// * it under the terms of the GNU Affero General Public License as
-// * published by the Free Software Foundation, either version 3 of the
-// * License, or (at your option) any later version.
-// *
-// * SwayDB is distributed in the hope that it will be useful,
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// * GNU Affero General Public License for more details.
-// *
-// * You should have received a copy of the GNU Affero General Public License
-// * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
-// */
-//
-//package swaydb.core.segment.merge
-//
-//import scala.util.Random
-//import swaydb.core.{TestBase, TestData}
-//import swaydb.core.data._
-//import swaydb.core.group.compression.data.{GroupByInternal.Groups, GroupByInternal.KeyValues}
-//import swaydb.core.util.Benchmark
-//import swaydb.data.order.{KeyOrder, TimeOrder}
-//import swaydb.data.slice.Slice
-//import swaydb.data.util.StorageUnits._
-//import swaydb.core.TestData._
-//import swaydb.core.CommonAssertions._
-//import swaydb.core.RunThis._
-//import swaydb.core.IOAssert._
-//
-//class SegmentMergeStressSpec extends TestBase {
-//
-//  implicit val keyOrder = KeyOrder.default
-//  implicit val timeOrder: TimeOrder[Slice[Byte]] = TimeOrder.long
-//
-//  val keyValueCount = 100
-//  val maxIteration = 5
-//
-//  /**
-//    * Result: Merging of grouped key-values is expensive and should be deferred to lower levels.
-//    *
-//    * Deeply nested groups should be avoided. So [[GroupByInternal.Groups]] should be avoided.
-//    */
-//  "Randomly merging overlapping key-values" should {
-//    "be performant" in {
-//      (1 to maxIteration).foldLeft(Slice.empty[KeyValue.ReadOnly]) {
-//        case (oldKeyValues, index) =>
-//          println(s"Iteration: $index/$maxIteration")
-//
-//          val newKeyValues = randomizedKeyValues(count = keyValueCount, startId = Some(index * 200))
-//
-//          val groupGroupBy =
-//            if (Random.nextBoolean())
-//              Some(
-//                GroupByInternal.Groups.Count(
-//                  count = randomInt(5) + 1,
-//                  sortedIndexCompression = randomCompression(),
-//                  valuesCompression = randomCompression()
-//                )
-//              )
-//            else if (Random.nextBoolean())
-//              Some(
-//                GroupByInternal.Groups.Size(
-//                  size = randomInt(500) + 200,
-//                  sortedIndexCompression = randomCompression(),
-//                  valuesCompression = randomCompression()
-//                )
-//              )
-//            else
-//              None
-//
-//          implicit val compression =
-//            if (Random.nextBoolean())
-//              Some(
-//                GroupByInternal.KeyValues.Count(
-//                  count = newKeyValues.size / randomInt(10) + 1,
-//                  groupCompression = groupGroupBy,
-//                  sortedIndexCompression = randomCompression(),
-//                  valuesCompression = randomCompression()
-//                )
-//              )
-//            else if (Random.nextBoolean())
-//              Some(
-//                GroupByInternal.KeyValues.Size(
-//                  size = newKeyValues.last.stats.segmentSizeWithoutFooter / randomInt(10) + 1,
-//                  groupCompression = groupGroupBy,
-//                  sortedIndexCompression = randomCompression(),
-//                  valuesCompression = randomCompression()
-//                )
-//              )
-//            else
-//              None
-//
-//          val mergedKeyValues =
-//            Benchmark("Merge performance") {
-//              SegmentMerger.merge(
-//                newKeyValues = newKeyValues,
-//                oldKeyValues = oldKeyValues,
-//                minSegmentSize = 100.mb,
-//                maxProbe = TestData.maxProbe,
-//                isLastLevel = false,
-//                forInMemory = false,
-//                bloomFilterFalsePositiveRate = TestData.falsePositiveRate,
-//                resetPrefixCompressionEvery = TestData.resetPrefixCompressionEvery,
-//                minimumNumberOfKeyForHashIndex = TestData.minimumNumberOfKeysForHashIndex,
-//                allocateSpace = TestData.allocateSpace,
-//                enableBinarySearchIndex = TestData.enableBinarySearchIndex,
-//                buildFullBinarySearchIndex = TestData.buildFullBinarySearchIndex,
-//                compressDuplicateValues = true
-//              ).runIO
-//            }
-//          mergedKeyValues should have size 1
-//          val head = mergedKeyValues.head.toSlice
-//          println(head.size)
-//          head
-//      }
-//    }
-//  }
-//}
+/*
+ * Copyright (c) 2019 Simer Plaha (@simerplaha)
+ *
+ * This file is a part of SwayDB.
+ *
+ * SwayDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * SwayDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package swaydb.core.segment.merge
+
+import swaydb.core.CommonAssertions._
+import swaydb.core.TestBase
+import swaydb.core.TestData._
+import swaydb.core.data._
+import swaydb.core.group.compression.GroupByInternal
+import swaydb.core.segment.format.a.block._
+import swaydb.core.util.Benchmark
+import swaydb.data.order.{KeyOrder, TimeOrder}
+import swaydb.data.slice.Slice
+import swaydb.data.util.StorageUnits._
+
+class SegmentMergeStressSpec extends TestBase {
+
+  implicit val keyOrder = KeyOrder.default
+  implicit val timeOrder: TimeOrder[Slice[Byte]] = TimeOrder.long
+
+  val keyValueCount = 1000
+  val maxIteration = 10
+
+  /**
+   * Result: Merging of grouped key-values is expensive and should be deferred to lower levels.
+   *
+   * Deeply nested groups should be avoided. So [[GroupByInternal.Groups]] should be avoided.
+   */
+  "Randomly merging overlapping key-values" should {
+    "be performant" in {
+      (1 to maxIteration).foldLeft(Slice.empty[KeyValue.ReadOnly]) {
+        case (oldKeyValues, index) =>
+          println(s"Iteration: $index/$maxIteration")
+
+          val newKeyValues = randomizedKeyValues(count = keyValueCount, startId = Some(index * 200), addGroups = false)
+
+          implicit val groupGroupBy = randomGroupByOption(10)
+
+          val mergedKeyValues =
+            Benchmark("Merge performance") {
+              SegmentMerger.merge(
+                newKeyValues = newKeyValues,
+                oldKeyValues = oldKeyValues,
+                minSegmentSize = 100.mb,
+                isLastLevel = false,
+                forInMemory = false,
+                createdInLevel = randomIntMax(),
+                valuesConfig = ValuesBlock.Config.random,
+                sortedIndexConfig = SortedIndexBlock.Config.random,
+                binarySearchIndexConfig = BinarySearchIndexBlock.Config.random,
+                hashIndexConfig = HashIndexBlock.Config.random,
+                bloomFilterConfig = BloomFilterBlock.Config.random,
+                segmentIO = SegmentIO.random
+              ).get
+            }
+
+          mergedKeyValues should have size 1
+          val head = mergedKeyValues.head.toSlice
+          println("Merged key-values: " + head.size)
+          head
+      }
+    }
+  }
+}
