bace90f2724cbd35237fc4e87381b7d14ebba42e
simerplaha <simer.j@gmail.com>
2020-01-23 15:00:19 +1100

    StreamSpec

--- swaydb/src/test/scala/swaydb/api/StreamSpec.scala
+++ swaydb/src/test/scala/swaydb/api/StreamSpec.scala
@@ -1,221 +1,221 @@
-///*
-// * Copyright (c) 2020 Simer JS Plaha (simer.j@gmail.com - @simerplaha)
-// *
-// * This file is a part of SwayDB.
-// *
-// * SwayDB is free software: you can redistribute it and/or modify
-// * it under the terms of the GNU Affero General Public License as
-// * published by the Free Software Foundation, either version 3 of the
-// * License, or (at your option) any later version.
-// *
-// * SwayDB is distributed in the hope that it will be useful,
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// * GNU Affero General Public License for more details.
-// *
-// * You should have received a copy of the GNU Affero General Public License
-// * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
-// */
-//
-//package swaydb.api
-//
-//import org.scalatest.{Matchers, WordSpec}
-//import swaydb.Bag._
-//import swaydb.core.RunThis._
-//import swaydb.{IO, Stream, Bag}
-//
-//import scala.concurrent.duration._
-//import scala.concurrent.{Await, Future}
-//import scala.util.Try
-//
-//class StreamFutureSpec extends StreamSpec[Future] {
-//  override def get[A](a: Future[A]): A = Await.result(a, 60.seconds)
-//}
-//
-//class StreamIOSpec extends StreamSpec[IO.ApiIO] {
-//  override def get[A](a: IO.ApiIO[A]): A = a.get
-//}
-//
-//class StreamTrySpec extends StreamSpec[Try] {
-//  override def get[A](a: Try[A]): A = a.get
-//}
-//
-//sealed abstract class StreamSpec[T[_]](implicit bag: Bag[T]) extends WordSpec with Matchers {
-//
-//  def get[A](a: T[A]): A
-//
-//  implicit class Get[A](a: T[A]) {
-//    def await = get(a)
-//  }
-//
-//  "Stream" should {
-//
-//    "empty" in {
-//      Stream.empty[Int, T]
-//        .map(_.toString)
-//        .materialize
-//        .await shouldBe empty
-//    }
-//
-//    "range" in {
-//      Stream
-//        .range[T](1, 100)
-//        .materialize
-//        .await shouldBe (1 to 100)
-//
-//      Stream
-//        .range[T]('a', 'z')
-//        .materialize
-//        .await shouldBe ('a' to 'z')
-//    }
-//
-//    "rangeUntil" in {
-//      Stream
-//        .rangeUntil[T](1, 100)
-//        .materialize
-//        .await shouldBe (1 to 99)
-//
-//      Stream
-//        .rangeUntil[T]('a', 'z')
-//        .materialize
-//        .await shouldBe ('a' to 'y')
-//    }
-//
-//    "tabulate" in {
-//      Stream
-//        .tabulate[Int, T](5)(_ + 1)
-//        .materialize
-//        .await shouldBe (1 to 5)
-//
-//      Stream
-//        .tabulate[Int, T](0)(_ + 1)
-//        .materialize
-//        .await shouldBe empty
-//    }
-//
-//    "headOption" in {
-//      Stream[Int, T](1 to 100)
-//        .headOption
-//        .await should contain(1)
-//    }
-//
-//    "count" in {
-//      Stream[Int, T](1 to 100)
-//        .count(_ % 2 == 0)
-//        .await shouldBe 50
-//    }
-//
-//    "lastOptionLinear" in {
-//      Stream[Int, T](1 to 100)
-//        .lastOption
-//        .await should contain(100)
-//    }
-//
-//    "map" in {
-//      Stream[Int, T](1 to 1000)
-//        .map(_ + " one")
-//        .map(_ + " two")
-//        .map(_ + " three")
-//        .materialize
-//        .await shouldBe (1 to 1000).map(_ + " one two three")
-//    }
-//
-//    "collect conditional" in {
-//      Stream[Int, T](1 to 1000)
-//        .collect { case n if n % 2 == 0 => n }
-//        .materialize
-//        .await shouldBe (2 to 1000 by 2)
-//    }
-//
-//    "collectFirst" in {
-//      Stream[Int, T](1 to 1000)
-//        .collectFirst { case n if n % 2 == 0 => n }
-//        .await should contain(2)
-//    }
-//
-//    "collect all" in {
-//      Stream[Int, T](1 to 1000)
-//        .collect { case n => n }
-//        .materialize
-//        .await shouldBe (1 to 1000)
-//    }
-//
-//    "drop, take and map" in {
-//      Stream[Int, T](1 to 20)
-//        .map(_.toString)
-//        .drop(10)
-//        .take(5)
-//        .map(_.toInt)
-//        .drop(2)
-//        .take(1)
-//        .materialize
-//        .await should contain only 13
-//    }
-//
-//    "drop, take and foreach" in {
-//      Stream[Int, T](1 to 1000)
-//        .map(_.toString)
-//        .drop(10)
-//        .take(1)
-//        .map(_.toInt)
-//        .materialize
-//        .await should have size 1
-//    }
-//
-//    "takeWhile" in {
-//      Stream[Int, T](1 to 1000)
-//        .map(_.toString)
-//        .drop(10)
-//        .takeWhile(_.toInt <= 10)
-//        .materialize
-//        .await shouldBe empty
-//    }
-//
-//    "dropWhile" in {
-//      Stream[Int, T](1 to 20)
-//        .map(_.toString)
-//        .drop(10)
-//        .dropWhile(_.toInt <= 20)
-//        .materialize
-//        .await shouldBe empty
-//
-//      Stream[Int, T](1 to 20)
-//        .map(_.toString)
-//        .drop(11)
-//        .dropWhile(_.toInt % 2 == 0)
-//        .materialize
-//        .await shouldBe (13 to 20).map(_.toString)
-//    }
-//
-//    "flatMap" in {
-//      Stream[Int, T](1 to 10)
-//        .flatMap(_ => Stream[Int, T](1 to 10))
-//        .materialize
-//        .await shouldBe Array.fill(10)(1 to 10).flatten
-//    }
-//
-//    "filter" in {
-//      Stream[Int, T](1 to 10)
-//        .map(_ + 10)
-//        .filter(_ % 2 == 0)
-//        .take(2)
-//        .materialize
-//        .await should contain only(12, 14)
-//    }
-//
-//    "filterNot" in {
-//      Stream[Int, T](1 to 10)
-//        .filterNot(_ % 2 == 0)
-//        .take(2)
-//        .materialize
-//        .await shouldBe (1 to 10).filter(_ % 2 != 0).take(2)
-//    }
-//
-//    "not stack overflow" in {
-//      Stream[Int, T](1 to 1000000)
-//        .filter(_ % 100000 == 0)
-//        .materialize
-//        .await should contain only(100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000)
-//    }
-//  }
-//}
+/*
+ * Copyright (c) 2020 Simer JS Plaha (simer.j@gmail.com - @simerplaha)
+ *
+ * This file is a part of SwayDB.
+ *
+ * SwayDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * SwayDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package swaydb.api
+
+import org.scalatest.{Matchers, WordSpec}
+import swaydb.Bag._
+import swaydb.core.RunThis._
+import swaydb.{IO, Stream, Bag}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.Try
+
+class StreamFutureSpec extends StreamSpec[Future] {
+  override def get[A](a: Future[A]): A = Await.result(a, 60.seconds)
+}
+
+class StreamIOSpec extends StreamSpec[IO.ApiIO] {
+  override def get[A](a: IO.ApiIO[A]): A = a.get
+}
+
+class StreamTrySpec extends StreamSpec[Try] {
+  override def get[A](a: Try[A]): A = a.get
+}
+
+sealed abstract class StreamSpec[T[_]](implicit bag: Bag[T]) extends WordSpec with Matchers {
+
+  def get[A](a: T[A]): A
+
+  implicit class Get[A](a: T[A]) {
+    def await = get(a)
+  }
+
+  "Stream" should {
+
+    "empty" in {
+      Stream.empty[Int]
+        .map(_.toString)
+        .materialize[T]
+        .await shouldBe empty
+    }
+
+    "range" in {
+      Stream
+        .range(1, 100)
+        .materialize[T]
+        .await shouldBe (1 to 100)
+
+      Stream
+        .range('a', 'z')
+        .materialize[T]
+        .await shouldBe ('a' to 'z')
+    }
+
+    "rangeUntil" in {
+      Stream
+        .rangeUntil(1, 100)
+        .materialize[T]
+        .await shouldBe (1 to 99)
+
+      Stream
+        .rangeUntil('a', 'z')
+        .materialize[T]
+        .await shouldBe ('a' to 'y')
+    }
+
+    "tabulate" in {
+      Stream
+        .tabulate[Int](5)(_ + 1)
+        .materialize[T]
+        .await shouldBe (1 to 5)
+
+      Stream
+        .tabulate[Int](0)(_ + 1)
+        .materialize[T]
+        .await shouldBe empty
+    }
+
+    "headOption" in {
+      Stream[Int](1 to 100)
+        .headOption[T]
+        .await should contain(1)
+    }
+
+    "count" in {
+      Stream[Int](1 to 100)
+        .count[T](_ % 2 == 0)
+        .await shouldBe 50
+    }
+
+    "lastOptionLinear" in {
+      Stream[Int](1 to 100)
+        .lastOption[T]
+        .await should contain(100)
+    }
+
+    "map" in {
+      Stream[Int](1 to 1000)
+        .map(_ + " one")
+        .map(_ + " two")
+        .map(_ + " three")
+        .materialize[T]
+        .await shouldBe (1 to 1000).map(_ + " one two three")
+    }
+
+    "collect conditional" in {
+      Stream[Int](1 to 1000)
+        .collect { case n if n % 2 == 0 => n }
+        .materialize[T]
+        .await shouldBe (2 to 1000 by 2)
+    }
+
+    "collectFirst" in {
+      Stream[Int](1 to 1000)
+        .collectFirst[Int, T] { case n if n % 2 == 0 => n }
+        .await should contain(2)
+    }
+
+    "collect all" in {
+      Stream[Int](1 to 1000)
+        .collect { case n => n }
+        .materialize[T]
+        .await shouldBe (1 to 1000)
+    }
+
+    "drop, take and map" in {
+      Stream[Int](1 to 20)
+        .map(_.toString)
+        .drop(10)
+        .take(5)
+        .map(_.toInt)
+        .drop(2)
+        .take(1)
+        .materialize[T]
+        .await should contain only 13
+    }
+
+    "drop, take and foreach" in {
+      Stream[Int](1 to 1000)
+        .map(_.toString)
+        .drop(10)
+        .take(1)
+        .map(_.toInt)
+        .materialize[T]
+        .await should have size 1
+    }
+
+    "takeWhile" in {
+      Stream[Int](1 to 1000)
+        .map(_.toString)
+        .drop(10)
+        .takeWhile(_.toInt <= 10)
+        .materialize[T]
+        .await shouldBe empty
+    }
+
+    "dropWhile" in {
+      Stream[Int](1 to 20)
+        .map(_.toString)
+        .drop(10)
+        .dropWhile(_.toInt <= 20)
+        .materialize[T]
+        .await shouldBe empty
+
+      Stream[Int](1 to 20)
+        .map(_.toString)
+        .drop(11)
+        .dropWhile(_.toInt % 2 == 0)
+        .materialize[T]
+        .await shouldBe (13 to 20).map(_.toString)
+    }
+
+    "flatMap" in {
+      Stream[Int](1 to 10)
+        .flatMap(_ => Stream[Int](1 to 10))
+        .materialize[T]
+        .await shouldBe Array.fill(10)(1 to 10).flatten
+    }
+
+    "filter" in {
+      Stream[Int](1 to 10)
+        .map(_ + 10)
+        .filter(_ % 2 == 0)
+        .take(2)
+        .materialize[T]
+        .await should contain only(12, 14)
+    }
+
+    "filterNot" in {
+      Stream[Int](1 to 10)
+        .filterNot(_ % 2 == 0)
+        .take(2)
+        .materialize[T]
+        .await shouldBe (1 to 10).filter(_ % 2 != 0).take(2)
+    }
+
+    "not stack overflow" in {
+      Stream[Int](1 to 1000000)
+        .filter(_ % 100000 == 0)
+        .materialize[T]
+        .await should contain only(100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000)
+    }
+  }
+}
