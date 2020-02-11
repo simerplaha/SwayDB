ba4b3ec0bf74f24a3332f2454cbedc2d965fe75b
simerplaha <simer.j@gmail.com>
2020-02-12 06:36:01 +1100

    printFileSize

--- core/src/main/scala/swaydb/core/io/file/Effect.scala
+++ core/src/main/scala/swaydb/core/io/file/Effect.scala
@@ -263,4 +263,27 @@ private[core] object Effect extends LazyLogging {
 
   def size(path: Path): Long =
     Files.size(path)
+
+  def printFilesSize(folder: Path, fileExtension: String) = {
+
+    def extensionFilter(path: Path, attributes: BasicFileAttributes) = {
+      val fileName = path.getFileName.toString
+      val isSST = fileName.contains(fileExtension)
+      //if (isSST) println(fileName)
+      isSST
+    }
+
+    val size =
+      Files
+        .find(folder, Int.MaxValue, extensionFilter)
+        .iterator()
+        .asScala
+        .foldLeft(0L)(_ + _.toFile.length())
+
+    //    val gb = BigDecimal(size / 1000000000.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    val mb = BigDecimal(size / 1000000.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+
+    //    println(s"${fileExtension.toUpperCase} files size: $gb gb - $mb mb - $size bytes")
+    println(s"${fileExtension.toUpperCase} files size: $mb mb - $size bytes\n")
+  }
 }
