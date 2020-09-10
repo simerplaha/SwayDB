ab7095a13364c8268c4a061c295b54251694ced4
simerplaha <simer.j@gmail.com>
2020-09-10 23:38:47 +1000

    #234 - PersistentMultiMap

---
+++ swaydb-java/src/main/scala/swaydb/java/persistent/PersistentMultiMap.scala
@@ -0,0 +1,341 @@
+/*
+ * Copyright (c) 2020 Simer JS Plaha (simer.j@gmail.com - @simerplaha)
+ *
+ * This file is a part of SwayDB.
+ *
+ * SwayDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * SwayDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Additional permission under the GNU Affero GPL version 3 section 7:
+ * If you modify this Program or any covered work, only by linking or
+ * combining it with separate works, the licensors of this Program grant
+ * you additional permission to convey the resulting work.
+ */
+
+package swaydb.java.persistent
+
+import java.nio.file.Path
+import java.time.Duration
+import java.util.Collections
+import java.util.concurrent.ExecutorService
+
+import swaydb.configs.level.DefaultExecutionContext
+import swaydb.core.build.BuildValidator
+import swaydb.core.util.Eithers
+import swaydb.data.DataType
+import swaydb.data.accelerate.{Accelerator, LevelZeroMeter}
+import swaydb.data.compaction.{LevelMeter, Throttle}
+import swaydb.data.config._
+import swaydb.data.order.KeyOrder
+import swaydb.data.slice.Slice
+import swaydb.data.util.Java.JavaFunction
+import swaydb.data.util.StorageUnits._
+import swaydb.java.data.slice.ByteSlice
+import swaydb.java.serializers.{SerializerConverter, Serializer => JavaSerializer}
+import swaydb.java.{KeyComparator, KeyOrderConverter, PureFunction, Return}
+import swaydb.persistent.DefaultConfigs
+import swaydb.serializers.Serializer
+import swaydb.{Apply, Bag}
+
+import scala.compat.java8.DurationConverters._
+import scala.compat.java8.FunctionConverters._
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.jdk.CollectionConverters._
+import scala.reflect.ClassTag
+
+object PersistentMultiMap {
+
+  final class Config[M, K, V, F](dir: Path,
+                                 private var mapSize: Int = 4.mb,
+                                 private var appliedFunctionsMapSize: Int = 4.mb,
+                                 private var clearAppliedFunctionsOnBoot: Boolean = false,
+                                 private var mmapMaps: MMAP.Map = DefaultConfigs.mmap(),
+                                 private var recoveryMode: RecoveryMode = RecoveryMode.ReportFailure,
+                                 private var mmapAppendix: MMAP.Map = DefaultConfigs.mmap(),
+                                 private var appendixFlushCheckpointSize: Int = 2.mb,
+                                 private var otherDirs: java.util.Collection[Dir] = Collections.emptyList(),
+                                 private var cacheKeyValueIds: Boolean = true,
+                                 private var shutdownTimeout: Duration = 30.seconds.toJava,
+                                 private var threadStateCache: ThreadStateCache = ThreadStateCache.Limit(hashMapMaxSize = 100, maxProbe = 10),
+                                 private var sortedKeyIndex: SortedKeyIndex = DefaultConfigs.sortedKeyIndex(),
+                                 private var randomKeyIndex: RandomKeyIndex = DefaultConfigs.randomKeyIndex(),
+                                 private var binarySearchIndex: BinarySearchIndex = DefaultConfigs.binarySearchIndex(),
+                                 private var mightContainKeyIndex: MightContainIndex = DefaultConfigs.mightContainKeyIndex(),
+                                 private var valuesConfig: ValuesConfig = DefaultConfigs.valuesConfig(),
+                                 private var segmentConfig: SegmentConfig = DefaultConfigs.segmentConfig(),
+                                 private var fileCache: FileCache.Enable = DefaultConfigs.fileCache(DefaultExecutionContext.sweeperEC),
+                                 private var memoryCache: MemoryCache = DefaultConfigs.memoryCache(DefaultExecutionContext.sweeperEC),
+                                 private var levelZeroThrottle: JavaFunction[LevelZeroMeter, FiniteDuration] = (DefaultConfigs.levelZeroThrottle _).asJava,
+                                 private var levelOneThrottle: JavaFunction[LevelMeter, Throttle] = (DefaultConfigs.levelOneThrottle _).asJava,
+                                 private var levelTwoThrottle: JavaFunction[LevelMeter, Throttle] = (DefaultConfigs.levelTwoThrottle _).asJava,
+                                 private var levelThreeThrottle: JavaFunction[LevelMeter, Throttle] = (DefaultConfigs.levelThreeThrottle _).asJava,
+                                 private var levelFourThrottle: JavaFunction[LevelMeter, Throttle] = (DefaultConfigs.levelFourThrottle _).asJava,
+                                 private var levelFiveThrottle: JavaFunction[LevelMeter, Throttle] = (DefaultConfigs.levelFiveThrottle _).asJava,
+                                 private var levelSixThrottle: JavaFunction[LevelMeter, Throttle] = (DefaultConfigs.levelSixThrottle _).asJava,
+                                 private var acceleration: JavaFunction[LevelZeroMeter, Accelerator] = (Accelerator.noBrakes() _).asJava,
+                                 private var byteComparator: KeyComparator[ByteSlice] = null,
+                                 private var typedComparator: KeyComparator[K] = null,
+                                 private var compactionEC: Option[ExecutionContext] = None,
+                                 private var buildValidator: BuildValidator = BuildValidator.DisallowOlderVersions(DataType.MultiMap),
+                                 keySerializer: Serializer[K],
+                                 mapKeySerializer: Serializer[M],
+                                 valueSerializer: Serializer[V],
+                                 functionClassTag: ClassTag[_]) {
+
+    def setMapSize(mapSize: Int) = {
+      this.mapSize = mapSize
+      this
+    }
+
+    def setAppliedFunctionsMapSize(size: Int) = {
+      this.appliedFunctionsMapSize = size
+      this
+    }
+
+    def setClearAppliedFunctionsOnBoot(clear: Boolean) = {
+      this.clearAppliedFunctionsOnBoot = clear
+      this
+    }
+
+    def setMmapMaps(mmapMaps: MMAP.Map) = {
+      this.mmapMaps = mmapMaps
+      this
+    }
+
+    def setRecoveryMode(recoveryMode: RecoveryMode) = {
+      this.recoveryMode = recoveryMode
+      this
+    }
+
+    def setMmapAppendix(mmapAppendix: MMAP.Map) = {
+      this.mmapAppendix = mmapAppendix
+      this
+    }
+
+    def setAppendixFlushCheckpointSize(appendixFlushCheckpointSize: Int) = {
+      this.appendixFlushCheckpointSize = appendixFlushCheckpointSize
+      this
+    }
+
+    def setOtherDirs(otherDirs: java.util.Collection[Dir]) = {
+      this.otherDirs = otherDirs
+      this
+    }
+
+    def setCacheKeyValueIds(cacheKeyValueIds: Boolean) = {
+      this.cacheKeyValueIds = cacheKeyValueIds
+      this
+    }
+
+    def setShutdownTimeout(duration: Duration) = {
+      this.shutdownTimeout = duration
+      this
+    }
+
+    def setThreadStateCache(threadStateCache: ThreadStateCache) = {
+      this.threadStateCache = threadStateCache
+      this
+    }
+
+    def setSortedKeyIndex(sortedKeyIndex: SortedKeyIndex) = {
+      this.sortedKeyIndex = sortedKeyIndex
+      this
+    }
+
+    def setRandomKeyIndex(randomKeyIndex: RandomKeyIndex) = {
+      this.randomKeyIndex = randomKeyIndex
+      this
+    }
+
+    def setBinarySearchIndex(binarySearchIndex: BinarySearchIndex) = {
+      this.binarySearchIndex = binarySearchIndex
+      this
+    }
+
+    def setMightContainKeyIndex(mightContainKeyIndex: MightContainIndex) = {
+      this.mightContainKeyIndex = mightContainKeyIndex
+      this
+    }
+
+    def setValuesConfig(valuesConfig: ValuesConfig) = {
+      this.valuesConfig = valuesConfig
+      this
+    }
+
+    def setSegmentConfig(segmentConfig: SegmentConfig) = {
+      this.segmentConfig = segmentConfig
+      this
+    }
+
+    def setFileCache(fileCache: FileCache.Enable) = {
+      this.fileCache = fileCache
+      this
+    }
+
+    def setMemoryCache(memoryCache: MemoryCache) = {
+      this.memoryCache = memoryCache
+      this
+    }
+
+    def setLevelZeroThrottle(levelZeroThrottle: JavaFunction[LevelZeroMeter, FiniteDuration]) = {
+      this.levelZeroThrottle = levelZeroThrottle
+      this
+    }
+
+    def setLevelOneThrottle(levelOneThrottle: JavaFunction[LevelMeter, Throttle]) = {
+      this.levelOneThrottle = levelOneThrottle
+      this
+    }
+
+    def setLevelTwoThrottle(levelTwoThrottle: JavaFunction[LevelMeter, Throttle]) = {
+      this.levelTwoThrottle = levelTwoThrottle
+      this
+    }
+
+    def setLevelThreeThrottle(levelThreeThrottle: JavaFunction[LevelMeter, Throttle]) = {
+      this.levelThreeThrottle = levelThreeThrottle
+      this
+    }
+
+    def setLevelFourThrottle(levelFourThrottle: JavaFunction[LevelMeter, Throttle]) = {
+      this.levelFourThrottle = levelFourThrottle
+      this
+    }
+
+    def setLevelFiveThrottle(levelFiveThrottle: JavaFunction[LevelMeter, Throttle]) = {
+      this.levelFiveThrottle = levelFiveThrottle
+      this
+    }
+
+    def setLevelSixThrottle(levelSixThrottle: JavaFunction[LevelMeter, Throttle]) = {
+      this.levelSixThrottle = levelSixThrottle
+      this
+    }
+
+    def setAcceleration(acceleration: JavaFunction[LevelZeroMeter, Accelerator]) = {
+      this.acceleration = acceleration
+      this
+    }
+
+    def setByteComparator(byteComparator: KeyComparator[ByteSlice]) = {
+      this.byteComparator = byteComparator
+      this
+    }
+
+    def setTypedComparator(typedComparator: KeyComparator[K]) = {
+      this.typedComparator = typedComparator
+      this
+    }
+
+    def setCompactionExecutionContext(executionContext: ExecutorService) = {
+      this.compactionEC = Some(ExecutionContext.fromExecutorService(executionContext))
+      this
+    }
+
+    def setBuildValidator(buildValidator: BuildValidator) = {
+      this.buildValidator = buildValidator
+      this
+    }
+
+    private val functions = swaydb.MultiMap.Functions[M, K, V, swaydb.PureFunction[K, V, Apply.Map[V]]]()(keySerializer, mapKeySerializer, valueSerializer)
+
+    def registerFunctions(functions: F*): Config[M, K, V, F] = {
+      functions.foreach(registerFunction(_))
+      this
+    }
+
+    def registerFunction(function: F): Config[M, K, V, F] = {
+      functions.register(PureFunction.asScala(function.asInstanceOf[swaydb.java.PureFunction[K, V, Return.Map[V]]]))
+      this
+    }
+
+    def get(): swaydb.java.MultiMap[M, K, V, F] = {
+      val comparator: Either[KeyComparator[ByteSlice], KeyComparator[K]] =
+        Eithers.nullCheck(
+          left = byteComparator,
+          right = typedComparator,
+          default = KeyComparator.lexicographic
+        )
+
+      val scalaKeyOrder: KeyOrder[Slice[Byte]] = KeyOrderConverter.toScalaKeyOrder(comparator, keySerializer)
+
+      val scalaMap =
+        swaydb.persistent.MultiMap[M, K, V, swaydb.PureFunction[K, V, Apply.Map[V]], Bag.Less](
+          dir = dir,
+          mapSize = mapSize,
+          appliedFunctionsMapSize = appliedFunctionsMapSize,
+          clearAppliedFunctionsOnBoot = clearAppliedFunctionsOnBoot,
+          mmapMaps = mmapMaps,
+          recoveryMode = recoveryMode,
+          mmapAppendix = mmapAppendix,
+          appendixFlushCheckpointSize = appendixFlushCheckpointSize,
+          otherDirs = otherDirs.asScala.toSeq,
+          cacheKeyValueIds = cacheKeyValueIds,
+          shutdownTimeout = shutdownTimeout.toScala,
+          acceleration = acceleration.asScala,
+          threadStateCache = threadStateCache,
+          sortedKeyIndex = sortedKeyIndex,
+          randomKeyIndex = randomKeyIndex,
+          binarySearchIndex = binarySearchIndex,
+          mightContainKeyIndex = mightContainKeyIndex,
+          valuesConfig = valuesConfig,
+          segmentConfig = segmentConfig,
+          fileCache = fileCache,
+          memoryCache = memoryCache,
+          levelZeroThrottle = levelZeroThrottle.asScala,
+          levelOneThrottle = levelOneThrottle.asScala,
+          levelTwoThrottle = levelTwoThrottle.asScala,
+          levelThreeThrottle = levelThreeThrottle.asScala,
+          levelFourThrottle = levelFourThrottle.asScala,
+          levelFiveThrottle = levelFiveThrottle.asScala,
+          levelSixThrottle = levelSixThrottle.asScala
+        )(keySerializer = keySerializer,
+          mapKeySerializer = mapKeySerializer,
+          valueSerializer = valueSerializer,
+          functions = functions,
+          functionClassTag = functionClassTag.asInstanceOf[ClassTag[swaydb.PureFunction[K, V, Apply.Map[V]]]],
+          bag = Bag.less,
+          byteKeyOrder = scalaKeyOrder,
+          compactionEC = compactionEC.getOrElse(DefaultExecutionContext.compactionEC),
+          buildValidator = buildValidator
+        )
+
+      swaydb.java.MultiMap[M, K, V, F](scalaMap)
+    }
+  }
+
+  def functionsOn[M, K, V](dir: Path,
+                           mapKeySerializer: JavaSerializer[M],
+                           keySerializer: JavaSerializer[K],
+                           valueSerializer: JavaSerializer[V]): Config[M, K, V, swaydb.java.PureFunction[K, V, Return.Map[V]]] =
+    new Config(
+      dir = dir,
+      mapKeySerializer = SerializerConverter.toScala(mapKeySerializer),
+      keySerializer = SerializerConverter.toScala(keySerializer),
+      valueSerializer = SerializerConverter.toScala(valueSerializer),
+      functionClassTag = ClassTag.Any.asInstanceOf[ClassTag[swaydb.PureFunction[K, V, Apply.Map[V]]]]
+    )
+
+  def functionsOff[M, K, V](dir: Path,
+                            mapKeySerializer: JavaSerializer[M],
+                            keySerializer: JavaSerializer[K],
+                            valueSerializer: JavaSerializer[V]): Config[M, K, V, Void] =
+    new Config[M, K, V, Void](
+      dir = dir,
+      mapKeySerializer = SerializerConverter.toScala(mapKeySerializer),
+      keySerializer = SerializerConverter.toScala(keySerializer),
+      valueSerializer = SerializerConverter.toScala(valueSerializer),
+      functionClassTag = ClassTag.Nothing.asInstanceOf[ClassTag[Void]]
+    )
+}
