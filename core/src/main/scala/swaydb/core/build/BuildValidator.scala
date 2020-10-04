fd00e5d2c30b7f92d5fd15f9dd135724b52c777e
simerplaha <simer.j@gmail.com>
2020-10-04 13:10:31 +1100

    fixed header

--- core/src/main/scala/swaydb/core/build/BuildValidator.scala
+++ core/src/main/scala/swaydb/core/build/BuildValidator.scala
@@ -17,9 +17,9 @@
  * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
  *
  * Additional permission under the GNU Affero GPL version 3 section 7:
- * If you modify this Program, or any covered work, by linking or combining
- * it with other code, such other code is not for that reason alone subject
- * to any of the requirements of the GNU Affero GPL version 3.
+ * If you modify this Program or any covered work, only by linking or combining
+ * it with separate works, the licensors of this Program grant you additional
+ * permission to convey the resulting work.
  */
 
 package swaydb.core.build
