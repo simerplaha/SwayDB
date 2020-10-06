8f7e09efb0bf6fba78b2c7cecc03596ade18e308
simerplaha <simer.j@gmail.com>
2020-10-07 01:58:33 +1100

    remove braces

--- core/src/main/scala/swaydb/core/util/English.scala
+++ core/src/main/scala/swaydb/core/util/English.scala
@@ -27,10 +27,9 @@ package swaydb.core.util
 private[swaydb] object English {
 
   def plural(count: Int, word: String): String =
-    if (count == 0 || count > 1) {
+    if (count == 0 || count > 1)
       word + "s"
-    } else {
+    else
       word
-    }
 
 }
