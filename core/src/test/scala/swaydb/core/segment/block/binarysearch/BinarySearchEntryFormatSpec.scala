f72571dcc29fbf618883f5a6f001a5da39781c60
simerplaha <simer.j@gmail.com>
2021-10-18 20:05:00 +1100

    Apache2 header

--- core/src/test/scala/swaydb/core/segment/block/binarysearch/BinarySearchEntryFormatSpec.scala
+++ core/src/test/scala/swaydb/core/segment/block/binarysearch/BinarySearchEntryFormatSpec.scala
@@ -1,25 +1,17 @@
 /*
- * Copyright (c) 2018 Simer JS Plaha (simer.j@gmail.com - @simerplaha)
+ * Copyright 2018 Simer JS Plaha (simer.j@gmail.com - @simerplaha)
  *
- * This file is a part of SwayDB.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * SwayDB is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * SwayDB is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
- *
- * Additional permission under the GNU Affero GPL version 3 section 7:
- * If you modify this Program or any covered work, only by linking or
- * combining it with separate works, the licensors of this Program grant
- * you additional permission to convey the resulting work.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package swaydb.core.segment.block.binarysearch
